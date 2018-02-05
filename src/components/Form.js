18c23d5cb214010bff95239f75c986a3e506c67f
Nikitos <fishbah4@meta.ua>
2018-02-05 18:25:02 +0200

    readme config

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,52 +6,52 @@ import swal from 'sweetalert'
 
 class Form extends Component {
 
-	constructor (props) {
-		super(props)
+  constructor (props) {
+    super(props)
 
-		this.addPost = this.addPost.bind(this)
+    this.addPost = this.addPost.bind(this)
 	}
 
-	addPost () {
-	  if (this.titleInp.value !== '') {
-			this.props.onAddPost(this.titleInp.value, this.bodyInp.value)
-			this.bodyInp.value = ''
-			this.titleInp.value = ''
-		} else {
-			swal("Did you type any title?", "Type something to the title input to add the post!", "warning")
-		}
-	}
+  addPost () {
+    if (this.titleInp.value !== '') {
+  	  this.props.onAddPost(this.titleInp.value, this.bodyInp.value)
+  	  this.bodyInp.value = ''
+  	  this.titleInp.value = ''
+  	} else {
+      swal("Did you type any title?", "Type something to the title input to add the post!", "warning")
+    }
+  }
 
-	render() {
-		return (
-			<div className="post-add-form">
-				<input 
-					type="text"
-				    ref={inp => this.titleInp = inp}
-					placeholder="Type title of your post..."
-				/>
-				<textarea 
-					placeholder="Type body of your post..."
-					ref={inp => this.bodyInp = inp}
-				/>
-				<button onClick={this.addPost}>
-					Add
-				</button>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="post-add-form">
+        <input 
+          type="text"
+          ref={inp => this.titleInp = inp}
+          placeholder="Type title of your post..."
+        />
+        <textarea 
+          placeholder="Type body of your post..."
+          ref={inp => this.bodyInp = inp}
+        />
+        <button onClick={this.addPost}>
+          Add
+        </button>
+      </div>
+    )
+  }
 }
 
 function mapStateToProps(state) {
-	return {}
+  return {}
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		onAddPost: (title, body) => {
-			dispatch(addPost(title, body))
-		}
-	}
+  return {
+    onAddPost: (title, body) => {
+      dispatch(addPost(title, body))
+    }
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Form)
